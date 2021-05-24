a4acda523f379857530d08992a2cb68d328d3a96
lahiru <#L0718003955h>
2021-05-24 16:57:54 +0530

    Add footer component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
 import './App.css';
-=======
+
 import './App.css';
 import Header from "./components/Header";
->>>>>>> Add header component
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Header/>
->>>>>>> Add header component
+      <hr/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <div>
+            <h1>Hi from footer component</h1>
+        </div>
+    )
+}
+export default Footer;
